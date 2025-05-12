e9fb90768d9c587d50c8e26c5e528eaa62ddf2a2
agnapo <210852730+agnapo@users.noreply.github.com>
2025-05-12 17:13:53 +0000

    Add Skills project and Program.cs with CalculateNumbers function

---
+++ code/Skills/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+void CalculateNumbers(int var1, int var2)
+{
+    int sum = var1 + var2;
+    int diff = var1 - var2;
+    int prod = var1 * var2;
+    double div = (double)var1 / var2;
+
+    Console.WriteLine($"Sum: {sum}");
+    Console.WriteLine($"Difference: {diff}");
+    Console.WriteLine($"Product: {prod}");
+    Console.WriteLine($"Division: {div}");
+}
